ae68ecfc140c70a38f2195b5a8eac45f537ad0f3
Wenqin <wenqinYe@users.noreply.github.com>
2019-04-18 00:12:45 -0400

    Updates

---
+++ precache-manifest.2893b00f41cde11fb1644f330dd4003d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04a727356cf160d659c3",
+    "url": "/okapi/static/css/main.75cc5a89.chunk.css"
+  },
+  {
+    "revision": "04a727356cf160d659c3",
+    "url": "/okapi/static/js/main.48f532e8.chunk.js"
+  },
+  {
+    "revision": "0b868587c8170620e2db",
+    "url": "/okapi/static/js/runtime~main.6e5ed1da.js"
+  },
+  {
+    "revision": "9e8edabb6eab1b5687b8",
+    "url": "/okapi/static/js/2.d509523c.chunk.js"
+  },
+  {
+    "revision": "2b3b4cf03dd1c16977134555e504804e",
+    "url": "/okapi/static/media/Okapi_logo.2b3b4cf0.svg"
+  },
+  {
+    "revision": "7eae1a5f290c64f6745f369952b581e1",
+    "url": "/okapi/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/okapi/precache-manifest.2893b00f41cde11fb1644f330dd4003d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/okapi/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
