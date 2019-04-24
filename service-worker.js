ca62b3404757a2b0dbdfc967ecbcdfdd9faa0ec6
Wenqin <wenqinYe@users.noreply.github.com>
2019-04-24 12:45:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/okapi/precache-manifest.86be63757f6ee134ec43741b58b90fab.js"
+  "/okapi/precache-manifest.ea822cac1597fc465f82c534010e26c1.js"
 );
 
 workbox.clientsClaim();
